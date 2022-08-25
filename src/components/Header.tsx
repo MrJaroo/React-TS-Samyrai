30148d3b31474e0e1c1a3b7dea2dfa12fe0b33e7
MrJaroo <mishadolininma@mail.ru>
2022-08-25 11:45:02 +0400

    new project

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Header = () =>{
+    return (
+        <header className='header'>
+            <img
+                src="https://avatars.mds.yandex.net/i?id=acd0007d112fd7ba71ca8dc993021c69-4580062-images-thumbs&n=13"
+                alt="avanger"/>
+        </header>
+    )
+}
---
+++ src/components/Profile.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img
+                    src="https://avatars.mds.yandex.net/i?id=62bab513451faf3f91c7f46dc51a2b4f-2806623-images-thumbs&n=13"
+                    alt=""/>
+            </div>
+            <div>
+                ava + dicription
+            </div>
+            <div>
+                My post
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
